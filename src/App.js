6e7423a90f27c75605b826fb6892c8d136d1e3ad
Berkay <berkaydemirbas@yahoo.com>
2021-04-13 09:47:37 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import Link from "react";
-import {warningMessage} from "../src/contents/content"
+import { warningMessage } from "../src/contents/content";
 // import firebase from "./firebase/firebase";
 
 function App() {
-
   // firebase
   //   .firestore()
   //   .collection('books')
@@ -30,69 +29,59 @@ function App() {
     require.context("./search_results", false, /\.(html|json|zip)$/)
   );
 
-  const updateInput = e => {
+  const updateInput = (e) => {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
   return (
     <div className="App">
-
-<form>
-     <input
-       type="text"
-       name="fullname"
-       placeholder="Full name"
-       onChange={updateInput}
-      />
+      <form>
+        <input
+          type="text"
+          name="fullname"
+          placeholder="Full name"
+          onChange={updateInput}
+        />
       </form>
 
-{!results ? 
-      results.map((data, i) => (
-        <div key={i} className='query_box'>
-        Search For "<h1>Başlık</h1>"
-          {data.map((data, i) => (
-            <ul key={i}>
-              <li>
-                <div className='query_box_title'>
-                  <span>{i}</span>
-                  <h2>{data.name}</h2>
-                </div>
-                {data.pageDate}
-                <br />  <br />
-                <a href={data.domain}>{data.domain}</a>
-              </li>
-            </ul>
-          ))}
+      {!results ? (
+        results.map((data, i) => (
+          <div key={i} className="query_box">
+            Search For "<h1>Başlık</h1>"
+            {data.map((data, i) => (
+              <ul key={i}>
+                <li>
+                  <div className="query_box_title">
+                    <span>{i}</span>
+                    <h2>{data.name}</h2>
+                  </div>
+                  {data.pageDate}
+                  <br /> <br />
+                  <a href={data.domain}>{data.domain}</a>
+                </li>
+              </ul>
+            ))}
+          </div>
+        ))
+      ) : (
+        <div className="query_box">
+          <div className="query_box_title">
+            <h2>{warningMessage}</h2>
+          </div>
         </div>
-      ))
-      :
-      <div className='query_box'>
-      <div className='query_box_title'> 
-      <h2>       
-     {warningMessage}
-       </h2>
-        </div>
-        </div>
-    }
-      {/*
-      IMAGES FOR SS
-       <div className="images">
+      )}
+
+      <div className="images">
         {images.map((data, i) => (
-          <div className='image-list-wrapper'>
+          <div className="image-list-wrapper">
             <img key={i} src={data.default} />
             <text> {data.default} </text>
           </div>
-        ))} */}
-
-      {/* {reports.map((data, i) => (
-          <a key={i} href={data.default} download>
-            {data.default}
-          </a>
-        ))} */}
-
-    </div >
+        ))}
+      </div>
+    </div>
   );
 }
 
